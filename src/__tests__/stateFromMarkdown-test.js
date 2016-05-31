f91c31c5f1e36509ab15f222baa01489c8f81853
gugl <guenter.glueck@gmail.com>
2016-05-31 16:59:43 +0200

    Added tests for bold and link

--- src/__tests__/stateFromMarkdown-test.js
+++ src/__tests__/stateFromMarkdown-test.js
@@ -14,6 +14,29 @@ describe('stateFromMarkdown', () => {
       [{text: 'Hello World', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []}]
     );
   });
+
+  it('should create bold style', () => {
+    let contentState = stateFromMarkdown('This is **bold**');
+    let rawContentState = convertToRaw(contentState);
+    let blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual(
+      [{text: 'This is bold', type: 'unstyled', depth: 0, inlineStyleRanges: [{length: 4, offset: 8, style: 'BOLD'}], entityRanges: []}]
+    );
+  });
+
+  it('should create a link block', () => {
+    let contentState = stateFromMarkdown('This is a [link](http://example.com)');
+    let rawContentState = convertToRaw(contentState);
+    let blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual(
+      [{text: 'This is a link', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: [{key: 0, offset: 10, length: 4}]}]
+    );
+
+    let entityMap = rawContentState.entityMap;
+    expect(entityMap['0']).toEqual(
+      {data: {url: 'http://example.com'}, mutability: 'MUTABLE', type: 'LINK'}
+    );
+  });
 });
 
 function removeKeys(blocks) {
